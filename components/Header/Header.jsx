550c1454c0990c121273bf3b80fcd404f1f45b88
rehfeld13 <rafaelrehfeld97@gmail.com>
2024-01-25 21:30:02 -0300

    Create HeaderBackground component

---
+++ components/Header/Header.jsx
@@ -0,0 +1,44 @@
+// NextJS
+import Link from "next/link";
+import Image from "next/image";
+
+ // Images
+import logoImg from '@/assets/logo.png'
+
+// Styles
+import styles from '@/components/Header/header.module.css'
+
+// Components
+import HeaderBackground from "../HeaderBackground/HeaderBackground";
+
+export default function Header(){
+  return(
+    <>
+      <HeaderBackground />
+
+      <header className={styles.header}>
+        <Link className={styles.logo} href={"/"}>
+          <Image 
+            src={logoImg} 
+            alt="A plate whit food on it"
+            width={80}
+            height={80}
+            priority
+          />
+          NextLevel Food
+        </Link>
+
+        <nav className={styles.nav}>
+          <ul>
+            <li>
+              <Link href={"meals"}>Browse Meals</Link>
+            </li>
+            <li>
+              <Link href={"community"}>Foodies Community</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  )
+}
