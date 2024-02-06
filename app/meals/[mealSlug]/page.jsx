6e19fa98c780439778d2e5624ae67edda8e36f3d
rafael.rehfeld <rafael.rehfeld@codificar.com.br>
2024-02-06 16:29:01 -0300

    Meal Slug Page

--- app/meals/[mealSlug]/page.jsx
+++ app/meals/[mealSlug]/page.jsx
@@ -1,8 +1,47 @@
+//NEXTJS
+import Image from 'next/image'
+import { notFound } from 'next/navigation'
+
+// STYLES
+import styles from './page.module.css'
+
+// DATA
+import { getMeal } from '@/lib/meals'
 
 export default function MealDetailsPage({ params }) {
+
+  const meal = getMeal(params.mealSlug)
+
+  if(!meal){
+    notFound()
+  }
+
   return(
-    <main>
-      <h1>{params.mealSlug}</h1>
-    </main>
+    <>
+      <header className={styles.header}>
+        <div className={styles.image}>
+          <Image src={meal.image} alt={meal.title} fill />
+        </div>
+
+        <div className={styles.headerText}>
+          <h1>{meal.title}</h1>
+
+          <p className={styles.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+
+          <p className={styles.summary}>
+            {meal.summary}
+          </p>
+        </div>
+      </header>
+
+      <main>
+        <p className={styles.instructions} dangerouslySetInnerHTML={{
+          __html: `${meal.instructions}`,
+        }}
+        ></p>
+      </main>
+    </>
   )
 }
